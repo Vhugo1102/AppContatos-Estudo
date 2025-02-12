ffb295c39d17df933c2e24430e0ee04f8f6192a9
Vitor Hugo da Silva <149020824+Vhugo1102@users.noreply.github.com>
2025-02-12 16:40:43 -0300

    Projeto Criado

---
+++ src/main/java/br/com/vhugo1102/AppContatos_Estudo/AppContatosEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.vhugo1102.AppContatos_Estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContatosEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContatosEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vhugo1102/AppContatos_Estudo/AppContatosEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vhugo1102.AppContatos_Estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppContatosEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
